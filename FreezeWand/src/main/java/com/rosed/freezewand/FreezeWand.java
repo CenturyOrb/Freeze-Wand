bb90c653dd6c1a7f42ffb046bf54da18f6fb164d
CenturyOrb <122176094+CenturyOrb@users.noreply.github.com>
2023-07-06 14:43:07 -0400

    added profileCurrentPlayers()
    Added profileCurrentPLayers(). Method scans current only players after the plugin starts OR reloads and adds those players to playerProfile map. Fixes the bug where the plugin doesn't work after plugin is reloaded.

--- FreezeWand/src/main/java/com/rosed/freezewand/FreezeWand.java
+++ FreezeWand/src/main/java/com/rosed/freezewand/FreezeWand.java
@@ -3,9 +3,13 @@ package com.rosed.freezewand;
 import com.rosed.freezewand.Command.FreezeWandCommand;
 import com.rosed.freezewand.Listener.FreezeWandListener;
 import com.rosed.freezewand.Listener.PlayerJoinListener;
+import com.rosed.freezewand.Profile.PlayerProfile;
 import com.rosed.freezewand.Profile.PlayerProfileManager;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.UUID;
 
 public final class FreezeWand extends JavaPlugin {
 
@@ -30,11 +34,23 @@ public final class FreezeWand extends JavaPlugin {
         // initialize managers
         playerProfileManager = new PlayerProfileManager();
 
+        profileCurrentPlayers();
+
         // register events
         Bukkit.getPluginManager().registerEvents(new PlayerJoinListener(playerProfileManager), this);
         Bukkit.getPluginManager().registerEvents(new FreezeWandListener(playerProfileManager), this);
 
         // register commands
         getCommand("freezewand").setExecutor(new FreezeWandCommand());
+    }
+
+    /**
+     * in case of server reload add back current players before reload
+     */
+    private void profileCurrentPlayers()   {
+        for (Player player : Bukkit.getOnlinePlayers())   {
+            UUID playerUUID = player.getUniqueId();
+            playerProfileManager.getPlayerProfiles().put(playerUUID, new PlayerProfile(playerUUID));
+        }
     }
 }
