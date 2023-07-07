4b7a90ddebb1758894e28088adc44e1db58b81c3
CenturyOrb <122176094+CenturyOrb@users.noreply.github.com>
2023-07-07 13:16:05 -0400

    removed silly import in PlayerQuitListener

--- FreezeWand/src/main/java/com/rosed/freezewand/Listener/PlayerQuitListener.java
+++ FreezeWand/src/main/java/com/rosed/freezewand/Listener/PlayerQuitListener.java
@@ -6,7 +6,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
-import java.util.UUID;
 
 public class PlayerQuitListener implements Listener {
 
